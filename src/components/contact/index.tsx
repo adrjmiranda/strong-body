0a60570d7562324105e5ea6b64a29ba3c3156bbc
Adriano Miranda <adrjmiranda@gmail.com>
2024-04-01 10:21:07 -0300

    chore: added contact area

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -4,7 +4,22 @@ import { FunctionComponent, ReactNode } from 'react';
 import style from './style.module.scss';
 
 const Contact: FunctionComponent = (): ReactNode => {
-	return <div>Contact</div>;
+	return (
+		<div className={style.contact}>
+			<div className='wrapper'>
+				<div className={style.content}>
+					<h2>Join us now to take a test</h2>
+					<p>Take advantage of the 30-day trial</p>
+					<form action='#'>
+						<input type='email' name='email' placeholder='Your best email' />
+						<button type='submit' className='button'>
+							Subscribe now
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
